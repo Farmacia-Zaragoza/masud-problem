ae84795df6ccea5a8cce62a8dc6c4c9fe0c16ec0
Onil <onilkabbo@gmail.com>
2019-07-30 21:33:51 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={`/img/images.jpeg`} className="App-logo" alt="logo" 
+            style={{
+              width: '300px',
+              height: '300px',
+              borderRadius: '500px'
+            }}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
